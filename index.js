c2814c1c9161ddfdb746d00bd464a03151b430bb
Daniel Moffat <danmofo@gmail.com>
2021-10-17 00:30:19 +0100

    --poll now works

--- index.js
+++ index.js
@@ -22,12 +22,11 @@ const RightmoveResultsParser = require('./rightmove-results-parser');
     const [nodeBinaryPath, scriptPath, ...args] = process.argv;
     const mode = args.includes('--poll') ? 'poll' : 'full';
     const propertyService = new PropertyService();
+    const maxDepth = mode === 'full' ? 99999 : 1;
 
     if(mode === 'full') {
         await propertyService.deleteExisting();
-
         await propertyService.initDatabase();
-
         await propertyService.saveSearch({
             url: 'https://www.rightmove.co.uk/property-to-rent/find.html?locationIdentifier=USERDEFINEDAREA%5E%7B%22id%22%3A%226964845%22%2C%20%22polylines%22%3A%22wsk~HvhiKqyjA~ayAsjFpo%7CA%60w%5EnpFpytClbs%40%7CfzCxgfAj_%40_xkJ%7D%7BaE%7Bu%7CCc%7DDrpd%40efw%40rvdAnlAhtcB%22%7D&maxBedrooms=2&maxPrice=900&minPrice=700&propertyTypes=flat&primaryDisplayPropertyType=flats&maxDaysSinceAdded=14&mustHave=&dontShow=student%2ChouseShare%2Cretirement&furnishTypes=&keywords=',
             label: 'Flats within 1-2 hours of Evesham.'
@@ -37,22 +36,22 @@ const RightmoveResultsParser = require('./rightmove-results-parser');
     const searches = await propertyService.listSearches();
 
     for(const search of searches) {
+        await fetchResultsForSearch(search).init();
 
-        const parser = new RightmoveResultsParser({
-            startingUrl: search.url,
-            searchId: search.id,
-            maxDepth: (() => {
-                if(mode === 'full') {
-                    // Arbitrary high number
-                    return 9999;
-                }
-                return 1;
-            })(),
-            mode: mode
-        });
-
-        await parser.init();
+        if(mode === 'poll') {
+            setInterval(async () => {
+                await fetchResultsForSearch(search).init();
+            }, 10000);
+        }
     }
 
+    function fetchResultsForSearch(search) {
+        return new RightmoveResultsParser({
+            startingUrl: search.url,
+            searchId: search.id,
+            maxDepth: maxDepth,
+            mode: mode
+        });
+    }
 
 })();
